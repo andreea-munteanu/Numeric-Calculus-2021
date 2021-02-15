fa4f28fe4239161533f1f1fe511bd27919681484
andreea-munteanu <57788684+andreea-munteanu@users.noreply.github.com>
2021-02-15 19:12:01 +0200

    ex3 chaotic energy

--- Tema1/ex3.py
+++ Tema1/ex3.py
@@ -1,8 +1,8 @@
 import math
 import random
 
-pi = math.pi             # 3.141592653589793
-pos_inf = float('inf')   # +∞
+pi = math.pi  # 3.141592653589793
+pos_inf = float('inf')  # +∞
 neg_inf = float('-inf')  # -∞
 
 
@@ -40,9 +40,8 @@ def generate_no_in_range():
     # values = random.sample(range(-7000, 7000), 10000)
     # print(values)
     for i in range(len(values)):
-        if -pi / 2 < values[i] < pi / 2:
-            pass
-        elif -pi / 2 < values[i] > pi / 2:
+        # if x[i] in (-pi/2, pi/2) --> okay; otherwise bring into interval:
+        if -pi / 2 < values[i] > pi / 2:
             values[i] = bring_in_interval(values[i])
         # separate cases for x = pi/2 and x = -pi/2:
         elif values[i] == pi / 2:
@@ -60,6 +59,67 @@ print(x)
 
 """
 
+epsilon = pow(10, -13)  # set precision as 10^(-13)
 
-def calculate_tangent():
-    pass
+
+def calculate_tangent_lorentz(x, epsilon):
+    """
+    b[0] = x/(1+)
+    a[1], a[2], ..., a[n] = -x^2
+    b[1] = 1+
+    b[2] = 3+ etc.
+    """
+    j = 0
+    a = x[1] * (-x[1])
+    b = x[j]
+    f = b
+    C = b
+    D = 0
+    mic = pow(10, -12)
+    if f == 0:
+        f = mic
+    C = f
+    D = 0
+    j = 1
+
+    # simulating a do-while:
+
+    D = b + a * D
+    if D == 0:
+        D = mic
+    C = b + (a / C)
+    if C == 0:
+        C = mic
+    D = 1 / D
+    delta_j = C * D
+    f = delta_j * f
+    j += 1
+
+    while abs((delta_j - 1)) >= epsilon:
+        D = b + a * D
+        if D == 0:
+            D = mic
+        C = b + (a / C)
+        if C == 0:
+            C = mic
+        D = 1 / D
+        delta_j = C * D
+        f = delta_j * f
+        j += 1
+
+
+"""
+     ____________________________________ TANGENT APPROXIMATION USING POLYNOMIALS _____________________________________
+     
+     tan(x) = x + (1/3)*(x^3) + (2/15)*(x^5) + (17/315)*(x^7) + (62/2835)*(x^9) <==> tan(x) = x + P(x^2) * (x^3)
+
+"""
+
+
+def polynomials_approximation(value: float):
+    # coefficients:
+    c1 = 0.33333333333333333     # 1/3
+    c2 = 0.133333333333333333    # 2/15
+    c3 = 0.053968253968254       # 17/315
+    c4 = 0.0218694885361552      # 62/2835
+    return value + c1 * pow(value, 3) + c2 * pow(value, 5) + c3 * pow(value, 7) + c4 * pow(value, 9)
