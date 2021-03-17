f7841e820a4cd3e7df15ce50ce58f3434ed2fb93
andreea-munteanu <57788684+andreea-munteanu@users.noreply.github.com>
2021-03-17 11:09:54 +0200

    Update typeA.py

--- Tema 3/typeA.py
+++ Tema 3/typeA.py
@@ -3,6 +3,9 @@ import re
 
 
 def read_A_from_file(file):
+    """
+    Implementation with dictionaries (i wish)
+    """
     f = open(file, "r")
     n = int(f.readline())  # matrix size
     # print("n =", n)  --> 2021
@@ -18,7 +21,6 @@ def read_A_from_file(file):
         if row in A.keys():
             if col in A.keys():
                 pass
-           pass
         # else:
         #     A[row] = {(val, col)}
         #print(A)
@@ -33,9 +35,38 @@ def read_A_from_file(file):
 
 
 def read_A(file):
-    f = open('a.txt', 'r').readlines()
-    lines = len(f)
+    """
+    Implementation with lists of lists.
+    """
+    f = open(file, "r")
+    n = int(f.readline())  # matrix size
+    # print("n =", n)  --> 2021
+    line = f.readline()
+    A = [[]] * n
+    count = 0
+    while line:
+        line_i = f.readline().split(', ')
+        val = float(line_i[0])
+        row, col = int(line_i[1]), int(line_i[2])
+        # print(count, val, row, col)
+        row_count = -1
+        for i in A:  # 'i' is a list containing tuples (col, val)
+            row_count += 1
+            if row == row_count:
+                found_col = False
+                for tup in i:
+                    # if we reached an already registered (row, col) position, we add 'val':
+                    if tup[0] == col:
+                        # we save new val, delete old (col, val) and add new tuple (col, new_val):
+                        val += tup[1]
+                        i.remove(tup)
+                        i.append((col, val))
+                # if col doesn't exist on row, we add the tuple (col, val) on row 'i':
+                if not found_col:
+                    i.append((col, val))
+    print(A)
+    f.close()
 
 
-read_A_from_file('a.txt')
+read_A('a.txt')
 # print(n, A)
