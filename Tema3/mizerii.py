6046369937410895402d19ae13e3cb9319770ba9
andreea-munteanu <57788684+andreea-munteanu@users.noreply.github.com>
2021-03-17 12:10:38 +0200

    verificari

--- Tema3/mizerii.py
+++ Tema3/mizerii.py
@@ -1,8 +1,8 @@
 A = [[1, 2, 3],
      [4, 5, 6],
      [7, 8, 9]]
-for i in A:
-    print(i)
+# for i in A:
+#     print(i)
     # output:
     # [1, 2, 3]
     # [4, 5, 6]
@@ -14,8 +14,8 @@ AA = [[(1, 102.5), (3, 2.5)],
       [(2, 1.3), (4, 101.3)],
       [(1, 0.73), (4, 1.5), (5, 102.23)]
       ]
-for i in AA:
-    print(i)
+# for i in AA:
+#     print(i)
     # output:
     # [(1, 102.5), (3, 2.5)]
     # [(1, 3.5), (2, 104.88), (3, 1.05), (5, 0.33)]
@@ -29,7 +29,8 @@ for i in AA:
     row_count += 1  # row in matrix
     for tup in i:
         if tup[0] == 1:  # col = 1
-            print("value at position (", row_count, ", ", tup[0], ") is ", tup[1])
+            pass
+            # print("value at position (", row_count, ", ", tup[0], ") is ", tup[1])
             # output: (toate valorile de pe coloana col)
             # value at position(0, 1) is 102.5
             # value at position(1, 1) is 3.5
@@ -42,9 +43,55 @@ for i in AA:
     row_count += 1  # row in matrix
     if row_count == desired_col:
         for tup in i:
-            print("value at position (", row_count, ", ", tup[0], ") is ", tup[1])
+            pass
+            # print("value at position (", row_count, ", ", tup[0], ") is ", tup[1])
             # output: (toate valorile de pe linia 3):
             # value at position(3, 2) is 1.3
             # value at position(3, 4) is 101.3
 
-          
+# check that A is written correctly from a.txt
+# our sample input file: our_sample.txt
+f = open('our_sample.txt', "r")
+n = int(f.readline())  # matrix size
+line = f.readline()
+print("n =", n)
+B = [[] for _ in range(n)]
+print(B)
+count = -1
+while line:  # for line in a.txt:
+    count += 1
+    line_i = f.readline().split(', ')
+    val = float(line_i[0])
+    row, col = int(line_i[1]), int(line_i[2])
+    print(count, val, row, col)
+    row_count = -1
+    for i in B:  # 'i' is a list containing tuples (col, val)
+        row_count += 1
+        if row == row_count:
+            found_col = False
+            for tup in i:
+                # if we reached an already registered (row, col) position:
+                if tup[0] == col:
+                    found_col = True
+                    # we save new val, delete old (col, val) and add new tuple (col, new_val):
+                    val += tup[1]
+                    i.remove(tup)
+                    i.append((col, float(val)))
+            # if col doesn't exist on row, we add the tuple (col, val) on row 'i':
+            if not found_col:
+                i.append((col, float(val)))
+        print(row_count, ": ", i)
+        # 0: [(0, 102.5), (2, 2.5)]
+        # 1: [(0, 3.5), (1, 104.88), (2, 1.05), (4, 0.33)]
+        # 2: [(2, 100.0)]
+        # 3: [(1, 1.3), (3, 101.3)]
+        # 4: [(0, 0.73), (3, 1.5), (4, 102.23)]
+        
+""" Nu mai ajunge aici pentru că își ia index out of range în while, god knows why"""       
+# printing B
+count = -1
+for i in A:
+    count += 1
+    if i:
+        print("line", count, ": ", i, end='\n')
+f.close()
