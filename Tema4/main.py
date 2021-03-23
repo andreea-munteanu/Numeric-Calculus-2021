abb249504768fea1089e6e3d7e6a99f7d1d74d6b
andreea-munteanu <57788684+andreea-munteanu@users.noreply.github.com>
2021-03-23 21:19:14 +0200

    uhhhh afișează niște nr???? idk

--- Tema4/main.py
+++ Tema4/main.py
@@ -1,3 +1,9 @@
+from math import sqrt
+import numpy as np
+from numpy import linalg
+from scipy.sparse import diags
+
+
 def extract_data_from_a(file):
     """
     Method for extracting input for tridiagonal matrix from input file 'a_i'.
@@ -59,20 +65,76 @@ def check_diagonal(diagonal, epsilon) -> bool:
     return True
 
 
-def gauss_seidel():
-    pass
+def gauss_seidel(a, b, c, f, epsilon):
+    """
+    Method for computing the Gauss Seidel iterative method for sparse linear system solution approximation.
+
+    :return:
+    """
+    # initially, xgs = 0:
+    xgs = [0 for _ in range(n)]
+    delta_x = 0.0
+    k = 0
+    kmax = 10000  # maximum 10000 iterations
+    while k < kmax:
+        print(k, ": ", sep='', end='')
+        delta_x = 0.0
+        for i in range(0, n):
+            sum1 = 0.0
+            sum2 = 0.0
+            for j in (1, i):
+                if j < i and j == i - q:
+                    sum1 += c[j] * xgs[j]
+                elif j > i and j == i + p:
+                    sum2 += b[j] * xgs[j]
+            i_xgs = xgs[i]
+            xgs[i] = (f[i] - sum1 - sum2) / a[i]
+            delta_x += (xgs[i] - i_xgs) ** 2  # delta_x = ||x_c - x_p||
+        delta_x = sqrt(delta_x)
+        if delta_x < epsilon or delta_x >= 10 ** 8:
+            break
+        k += 1
+    print(f'Number of iterations: {k}')
+    if delta_x >= epsilon:
+        raise Exception("Divergence")
+    else:
+        # x_c is the approximation of the exact solution
+        return xgs
+
+
+def check_solution(x_gs, f, a, b, c, p, q):
+    """
+    Method for determining whether our computed solution 'sol' is correct.
+    We will compute the norm ||A * x_GS - f||∞.
+
+    :param x_gs: computed solution
+    :param a: main diagonal
+    :param b: diagonal starting at q
+    :param c: diagonal starting at p
+    :param p: index p
+    :param q: index q
+    :return: true if sol is correct with error epsilon, false otherwise
+    """
+    diagonals = [a, b, c]
+    diagonals = diags(diagonals, [0, p, -q]).toarray()
+    prod = diagonals.dot(x_gs)       # A * x_GS
+    res = prod.subtract(f)           # A * x_GS - f
+    return linalg.norm(res, np.inf)  # ||A * x_GS - f||∞
 
 
 if __name__ == '__main__':
     # extracting data from input files a_i:
     for i in range(1, 6):
+        print(f'\nIteration for files a{i}, f{i}:\n___________________________')
         n, p, q, a, b, c = extract_data_from_a(f'a{i}.txt')
         # computation error epsilon = 10 ^ (-p)
         eps = 10 ** (-p)
-        # we only move forward if the main diagonal contains non-zero values exclusively:
+        # only move forward if main diagonal contains non-zero values only:
         if check_diagonal(a, eps):
             f = extract_f(f'f{i}.txt')
-            # print(n, p, q, len(a), len(b), len(c), len(f))
+            x_GS = gauss_seidel(a, b, c, f, eps)
+            print(f'Checking solution: {check_solution(x_GS, f, a, b, c, p, q)}')
+            # print("x_GS: ", x_GS)
         else:
             print("Main diagonal has 0 values. "
                   "The system cannot be solved using successive over-relaxation iterative method.")
